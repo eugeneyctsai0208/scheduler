07574e14e9a0d9ec3925666f8b218c1637fbe052
Yu-Chian Tsai <eugeneyc@umich.edu>
2020-05-05 03:16:35 -0400

    added basic interface using switch case, added some goals to be done

--- scheduler.py
+++ scheduler.py
@@ -1,19 +1,63 @@
 import sqlite3
 from schedulersql import *
 
-def restore_db_memory(cursor):
-    reinitialize_table(cursor, "employee")
+def restore_db(cursor):
+    return reinitialize_table(cursor, "employee")
 
-def add_employee(cursor, fname, lname):
-    insert_to_table(cursor, "employee", [fname, lname])
+def add_employee(cursor, content):
+    return insert_to_table(cursor, "employee", content)
 
-def remove_employee(cursor, fname, attribute):
-    remove_from_table(cursor, "employee", fname, attribute)
+def remove_employee(cursor, content, attribute):
+    return remove_from_table(cursor, "employee", content, attribute)
 
 def retrieve_employees(cursor):
     return fetch_table(cursor, "employee")
 
-def prompt_command():
+def create_new_schedule(cursor):
+    pass
+
+def print_last_schedule(cursor):
+    pass
+
+def job_create_new_schedule(cursor):
+    pass
+
+
+def job_print_last_schedule(cursor):
+    pass
+
+
+def job_retrieve_employees(cursor):
+    return retrieve_employees(cursor)
+
+
+def job_add_employee(cursor):
+    fname = input("Employee First Name: ")
+    lname = input("Employee Last Name: ")
+    return add_employee(cursor, [fname, lname])
+
+
+def job_remove_employee(cursor):
+    fname = input("Employee First Name: ")
+    lname = input("Employee Last Name: ")
+    return remove_employee(cursor, [fname, lname], ["fname", "lname"])
+
+
+def job_restore_db(cursor):
+    return restore_db(cursor)
+
+
+def job_quit(cursor):
+    print("Saving data and quitting program...")
+    return 2
+
+
+def invalid_cmd(cursor):
+    print("Invalid Command")
+    return 0
+
+
+def prompt_command(cursor):
     print("Welcome to scheduler:")
     print("The following commands are available:")
     print("1. Create New Schedule")
@@ -21,13 +65,40 @@ def prompt_command():
     print("3. See List Of Employee")
     print("4. Add Employee")
     print("5. Remove Employee")
+    print("6. Clear Employees")
+    print("7, To Quit Program")
+    cmd = int(input(": "))
+    cmd_switcher = {
+        1: job_create_new_schedule,
+        2: job_print_last_schedule,
+        3: job_retrieve_employees,
+        4: job_add_employee,
+        5: job_remove_employee,
+        6: job_restore_db,
+        7: job_quit
+    }
+    func = cmd_switcher.get(cmd, lambda: invalid_cmd)
+    ret = func(cursor)
+    if ret == 1:
+        print("Command failed")
+    return ret
+
 
 if __name__ == "__main__":
     connection = sqlite3.connect("table.db")
     cursor = connection.cursor()
-
-    while True:
-        return_status = prompt_command()
+    return_status = 0
+    while return_status != 2:
+        return_status = prompt_command(cursor)
 
     connection.commit()
     connection.close()
+
+
+# TODO
+# * Change first name last name system to name string system
+# Add a search by attribute functionality
+# Add a create table by given attribute functionality (REACH)
+# Figure how to randomly assign slots
+# * Add a employee serial number
+# * Change delete by selected attribute
--- schedulersql.py
+++ schedulersql.py
@@ -1,28 +1,44 @@
 import sqlite3
 
-# cursor.execute(insert_command.format("Eugene", "Tsai"))
-# cursor.execute(insert_command.format("Eudora", "Tsai"))
-# cursor.execute(insert_command.format("Vincent", "Tsai"))
-# cursor.execute(insert_command.format("Sumiaty", "Widjaja"))
-
-
 def reinitialize_table(cursor, tablename):
     drop_command = "DROP TABLE {};"
     init_employee_command = """CREATE TABLE employee (
     fname VARCHAR(20),
     lname VARCHAR(20)
     );"""
-    cursor.execute(drop_command.format(tablename))
-    cursor.execute(init_employee_command)
+    try:
+        cursor.execute(drop_command.format(tablename))
+        print("Dropped database {}".format(tablename))
+    except:
+        print("Existing database not found! Proceed to creating...")
+        pass
+    try:
+        cursor.execute(init_employee_command)
+        print("Created TABLE employee: (first_name, last_name)")
+    except:
+        return 1
+    return 0
 
 def fetch_table(cursor, tablename):
-    cursor.execute("""SELECT * FROM {};""".format(tablename))
-    return cursor.fetchall()
+    try:
+        cursor.execute("""SELECT * FROM {};""".format(tablename))
+    except:
+        return 1
+    print(cursor.fetchall())
+    return 0
 
 def insert_to_table(cursor, tablename, content):
     insert_command = """INSERT INTO {} VALUES ("{}", "{}");"""
-    cursor.execute(insert_command.format(tablename, content[0], content[1]))
+    try:
+        cursor.execute(insert_command.format(tablename, content[0], content[1]))
+    except:
+        return 1
+    return 0
 
 def remove_from_table(cursor, tablename, content, attribute):
-    remove_command = """DELETE FROM {} WHERE {}='{}';"""
-    cursor.execute()
+    remove_command = """DELETE FROM {} WHERE {}='{}' AND {}='{}';"""
+    try:
+        cursor.execute(remove_command.format(tablename, attribute[0], content[0], attribute[1], content[1]))
+    except:
+        return 1
+    return 0
